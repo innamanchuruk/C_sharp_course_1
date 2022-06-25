406a836e9b9408c64320ad37b959771f3a113bc2
inna <inna-portnoi@mail.ru>
2022-06-25 12:06:39 +0300

    task1_1

--- Lesson_1/1_1/Program.cs
+++ Lesson_1/1_1/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hello, all people!");
+﻿// string num_str = Console.Readline();
+// int num = int.Parse(num_str);
+
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+
+if (num2 == num1 * num2)
+{
+    Console.WriteLine("Yes");
+
+}
+else
+{
+    Console.WriteLine("No");
+}
